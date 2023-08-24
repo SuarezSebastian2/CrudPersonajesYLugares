c6517318c2b08c1ec211f69b1a151b6427e20eeb
SuarezSebastian2 <suarezsebastian98@hotmail.com>
2023-08-23 21:02:58 -0500

    Fix error

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS 
+from flask_cors import CORS
+from sqlalchemy import asc
 
 app = Flask(__name__)
-CORS(app, origins=['http://127.0.0.1:5500']) # Esto es para que reciva peticiones desde el frontend
+CORS(app, origins=['http://127.0.0.1:5500']) # Esto es para que reciba peticiones desde el frontend
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost/PruebaABC'
 db = SQLAlchemy(app)
 
 class Location(db.Model):
     __tablename__ = 'lugares'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     nombre = db.Column(db.String(255))
     descripcion = db.Column(db.Text)
     characters = db.relationship('Character', backref='origin')
 
 class Character(db.Model):
     __tablename__ = 'personajes'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     nombre = db.Column(db.String(255))
     edad = db.Column(db.Integer)
     origen_id = db.Column(db.Integer, db.ForeignKey('lugares.id'))
 
+#funcion para encontrar id disponible
+def encontrar_id_disponible(model):
+    ids_existentes = [record[0] for record in db.session.query(model.id).order_by(asc(model.id)).all()]
+    id_disponible = 1
+    for id_existente in ids_existentes:
+        if id_existente != id_disponible:
+            break
+        id_disponible += 1
+    return id_disponible
+
 @app.route('/character', methods=['POST'])
 def add_character():
     data = request.json
-    new_character = Character(nombre=data['nombre'], edad=data['edad'], origen_id=data['origen_id'])
+    id_disponible = encontrar_id_disponible(Character)
+    new_character = Character(id=id_disponible, nombre=data['nombre'], edad=data['edad'], origen_id=data['origen_id'])
     db.session.add(new_character)
     db.session.commit()
     return jsonify({'message': 'Character added'}), 201
@@ -55,7 +67,8 @@ def delete_character(id):
 @app.route('/location', methods=['POST'])
 def add_location():
     data = request.json
-    new_location = Location(nombre=data['nombre'], descripcion=data['descripcion'])
+    id_disponible = encontrar_id_disponible(Location)
+    new_location = Location(id=id_disponible, nombre=data['nombre'], descripcion=data['descripcion'])
     db.session.add(new_location)
     db.session.commit()
     return jsonify({'message': 'Location added'}), 201
@@ -85,4 +98,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
